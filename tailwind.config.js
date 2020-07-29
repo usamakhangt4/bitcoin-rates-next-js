bc60f010a21568166de35fa6b49f498a351f1ada
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-07-29 16:42:23 +0500

    l

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        Arvo:['Arvo', 'serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+};
