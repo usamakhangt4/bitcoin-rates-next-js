306af03704675d3aefd2745e966a61b8bff287d3
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-07-31 15:38:45 +0500

    commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+      <span>hi im index</span>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        Arvo:['Arvo','serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
